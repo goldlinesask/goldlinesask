1d42d585b27c497f1d5ac4f6c5f2da19de4dd346
Gaby Andrade <gaby.andrade@doordash.com>
2019-10-08 20:06:16 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import logo from '../assets/images/logo.svg'
+
+const Header = props => (
+  <header id="header" className="alt">
+    <span className="logo">
+      <img src={logo} alt="" />
+    </span>
+    <h1>Gold Lines Ask</h1>
+    <p>Salary Negotiation</p>
+  </header>
+)
+
+export default Header
