ddc500ccbb85afa2b8dca8481011bed32dc5cfcc
Gaby Andrade <gabygandrade@gmail.com>
2020-01-04 12:03:09 -0800

    Style testimonials section

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -8,40 +8,36 @@ const Testimonials = props => (
     </header>
     <ul className="features">
       <li>
-        <h3>Yanare</h3>
-        <h4>Software Engineer II</h4>
         <p>
           "....Taylor & Gaby were harder on me than the actual recruiter, so by the
           time I had the conversation I was so nervous about, I was so ready..."
         </p>
+        <i className="name">Yanare, Software Engineer II</i>
       </li>
       <li>
-        <h3>Vanessa</h3>
-        <h4>Full-Stack/UI Engineer</h4>
         <p>
-          "The negotiation process was very difficult and slightly confusing, but I was happy to have Gaby to turn to. She helped me evaluate two offers I was torn between and <strong>gave me the confidence to continue forward</strong> with my decision, and even ask for a <strong>$10K signing bonus!</strong>"
+          "The negotiation process was very difficult and slightly confusing, but I was happy to have Gaby to turn to. She helped me evaluate two offers I was torn between and <span>gave me the confidence to continue forward</span> with my decision, and even ask for a <span>$10K signing bonus!</span>"
         </p>
+        <i className="name">Vanessa, Full-Stack/UI Engineer</i>
       </li>
       <li>
-        <h3>Another Person</h3>
         <p>
           "....Taylor & Gaby were harder on me than the actual recruiter, so by the
           time I had the conversation I was so nervous about, I was so ready..."
         </p>
+        <i className="name">Another person</i>
       </li>
       <li>
-        <h3>Michelle</h3>
-        <h4>Support Engineer</h4>
         <p>
-          "Gaby and Taylor gave me the confidence I needed to negotiate my first job offer in tech! Through their workshop, I learned how to navigate my first offer and successfully ask for more. I negotiated a <strong>X% increase over my initial offer</strong> and it would not have been possible without Gaby and Taylor! <strong>I’m so happy they gave me the guidance I needed!</strong>"
+          "Gaby and Taylor gave me the confidence I needed to negotiate my first job offer in tech! Through their workshop, I learned how to navigate my first offer and successfully ask for more. I negotiated a <span>X% increase over my initial offer</span> and it would not have been possible without Gaby and Taylor! <span>I’m so happy they gave me the guidance I needed!</span>"
         </p>
+        <i className="name">Michelle, Support Engineer</i>
       </li>
       <li>
-        <h3>Kristen</h3>
-        <h4>Full-Stack/UI Engineer</h4>
         <p>
-          "The idea of negotiating was very scary and overwhelming, but Taylor made the process manageable. She patiently and thoroughly helped me shore up my weaknesses and taught me techniques to <strong>prepare for every possible outcome</strong>. I really appreciated the methodical way she lowered my anxiety. In the end, I successfully negotiated <strong>$5K more in base salary and a $10K signing bonus.</strong>"
+          "The idea of negotiating was very scary and overwhelming, but Taylor made the process manageable. She patiently and thoroughly helped me shore up my weaknesses and taught me techniques to <span>prepare for every possible outcome</span>. I really appreciated the methodical way she lowered my anxiety. In the end, I successfully negotiated <span>$5K more in base salary and a $10K signing bonus.</span>"
         </p>
+        <i className="name">Kristen, Full-Stack/UI Engineer</i>
       </li>
     </ul>
   </section>
